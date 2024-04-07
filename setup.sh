c7c6fc24010e6b1bf0f1594f3d1c64d16a03283a
Anggahrm <58013844+Anggahrm@users.noreply.github.com>
2024-04-07 11:46:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt update && apt upgrade
-apt install imagemagick curl ffmpeg webp -y
+apt install git imagemagick curl ffmpeg webp -y
 curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && \
 apt-get install -y nodejs
 npm i && npm i pm2 npm@latest -g
