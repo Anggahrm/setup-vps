f9b8d2ed9a8704827dcfefcb787d58123735c08a
Anggahrm <58013844+Anggahrm@users.noreply.github.com>
2024-07-06 11:05:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
-apt update && apt upgrade
-apt install imagemagick curl ffmpeg webp -y
+#!/bin/bash
+
+# Update package list and upgrade all packages
+apt update && apt upgrade -y
+
+# Install necessary packages
+apt install -y imagemagick curl ffmpeg webp
+
+# Install Node.js LTS
 curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && \
 apt-get install -y nodejs
+
+# Install pm2 and npm globally
 npm i pm2 npm@latest -g
