54d278b7d349d36244c3db4ba6faff69b67c2053
Anggahrm <58013844+Anggahrm@users.noreply.github.com>
2024-12-22 12:10:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Update package list and upgrade all packages
-apt update && apt upgrade -y
+# Update package list and install necessary tools
+sudo apt update && \
+sudo apt install -y wget git ffmpeg webp imagemagick unzip zip neofetch g++ make speedtest-cli && \
 
-# Install necessary packages
-apt install -y imagemagick curl ffmpeg webp python3 python3-pip
+# Download and install NVM (Node Version Manager)
+wget https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh && \
+bash install.sh && \
+source ~/.bashrc && \
 
-# Install Node.js LTS
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && \
-apt-get install -y nodejs
+# Install Node.js version 20
+nvm install v20 && \
 
-# Install pm2 and npm globally
-npm i pm2 npm@latest -g
+# Install PM2 globally and FFmpeg for PM2
+npm i pm2 -g && \
+pm2 install ffmpeg && \
+
+# Install gyp globally
+npm i gyp -g && \
+
+# Display system information
+neofetch
